9fe26bf80defba36912340e6341bec6660c23023
LinaUseche <lmuseche2@misena.edu.co>
2018-08-23 23:08:27 -0500

    A commit

---
+++ views/front/templates/foot.php
@@ -0,0 +1,30 @@
+<center>
+	<div class="footer">
+		<div class="grafico">
+		<img src="images/grafico.png" width="115%">
+		</div>
+		<div class="redes">
+		<img src="images/redes.png" width="90%">
+		</div>
+	</div>
+<!--Estrellas-->
+<form class="form">
+	<input class="1" type="radio">
+	<label for="1">★</label>
+
+	<input class="2" type="radio">
+	<label for="2">★</label>
+
+	<input class="3" type="radio">
+	<label for="3">★</label>
+
+	<input class="4" type="radio">
+	<label for="4">★</label>
+
+	<input class="4" type="radio">
+	<label for="4">★</label>
+</form>
+	</div>
+<script type="text/javascript" src="http://localhost/login/public/js/navbar.js"></script>
+</body>
+</html>
---
+++ views/front/templates/nav.php
@@ -0,0 +1,7 @@
+<div class="topnav" id="myTopnav">
+	<a href="#" class="active">Login PHP-PDO</a>
+	<a href="http://localhost/login/views/front/index.php">Inicio</a>
+	<a href="http://localhost/login/views/front/pages/login.php"></a>
+	<a href="register.php">Registro</a>
+	<a href="javascript:void(0);" style="font-size: 15px;" class="icon" onclick="mtFunction()">&#9776;</a>
+</div>
